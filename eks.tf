a9aa8b005fc6d36ed7cdcb78092c1316a113ad9d
chris <Intimidation1@hotmail.com>
2024-06-06 00:40:24 -0400

    adding node group

--- eks.tf
+++ eks.tf
@@ -11,5 +11,4 @@ module "eks" {
     vpc-cni    = {}
   }
 }
-## eks.tf is the main file that will be used to create the EKS cluster
 // this file is using the terraform-aws-modules/eks/aws module to create the EKS cluster
