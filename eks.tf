1ee5cdca3606ce2a981345e71322193d232160cc
chris <Intimidation1@hotmail.com>
2024-07-05 23:47:30 -0400

    adding vpc-cni cluster addon

--- eks.tf
+++ eks.tf
@@ -2,7 +2,7 @@ module "eks_al2" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = local.cluster_name
+  cluster_name    = "${local.name}-al2"
   cluster_version = "1.30"
 
   # EKS Addons
