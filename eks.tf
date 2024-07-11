e7760a6df12b74d1af2349b0e761bbaaf018b4d6
chris <Intimidation1@hotmail.com>
2024-07-11 16:35:36 -0400

    updating eks module

--- eks.tf
+++ eks.tf
@@ -1,4 +1,74 @@
-module "eks_al2" {
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = local.name
+  cluster_version = "1.30"
+
+  # EKS Addons
+  cluster_addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {}
+    kube-proxy             = {}
+    vpc-cni                = {}
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    example = {
+      ami_type       = "AL2_x86_64"
+      instance_types = ["t2.large"]
+
+      min_size = 1
+      max_size = 2
+      # This value is ignored after the initial creation
+      # desired_size = 2
+    }
+  }
+  # Cluster access entry
+  # To add the current caller identity as an administrator
+  enable_cluster_creator_admin_permissions = true
+
+  access_entries = {
+    # One access entry with a policy associated
+    example = {
+      kubernetes_groups = []
+      principal_arn     = "arn:aws:iam::123456789012:role/something"
+
+      policy_associations = {
+        example = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
+          access_scope = {
+            namespaces = ["default"]
+            type       = "namespace"
+          }
+        }
+      }
+    }
+  }
+
+
+  tags = {
+    Environment = "test"
+    Terraform   = "true"
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*module "eks_al2" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
@@ -32,4 +102,4 @@ module "eks_al2" {
   enable_cluster_creator_admin_permissions = true
 
   tags = local.tags
-}
+}*/
